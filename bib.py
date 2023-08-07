cf456ff8dfb19307f54de7e5a2138bc908a94677
luanacarlos <luan.alencar@icomp.ufam.edu.br>
2023-08-07 10:11:25 -0400

    Atualizando bib

--- bib.py
+++ bib.py
@@ -1,3 +1,4 @@
+from numpy import sort
 def media_aritmetica(dados):
     soma = 0
     for i in dados:
@@ -5,4 +6,13 @@ def media_aritmetica(dados):
 
     return soma/len(dados) if len(dados) > 0 else 0
 
-print(media_aritmetica([]))
+def mediana(dados):
+    dados.sort()
+    if len(dados)%2 == 0:
+        return media([dados[len(dados)/2],dados[(len(dados)/2)+1]])  
+    else:
+        return dados(len)
+
+
+
+mediana([1,2,3,4,5,5,54,4,2,1,])
