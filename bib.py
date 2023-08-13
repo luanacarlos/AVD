9c16b2114f1857f2daf58371ac8e2fb15cc3ad95
luanacarlos <luan.alencar@icomp.ufam.edu.br>
2023-08-13 15:51:23 -0400

    implementando moda

--- bib.py
+++ bib.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import *
 import matplotlib.pyplot as plt
+from collections import Counter
 
 
 def media_aritmetica(dados):
@@ -22,30 +23,34 @@ def mediana(dados):
 def media_ponderada(pesos, dados):
     numerador = sum(list(map(lambda x, y: x*y, pesos, dados)))
     media = numerador/sum(pesos)
-    print(round(media, 1))
+    return(round(media, 1))
 
 
 def media_geometrica(dados):
     media = np.prod(dados)**(1/len(dados))
-    print(round(media, 1))
+    return(round(media, 1))
 
 
 def media_harmonica(dados):
     numerador = len(dados)
     denominador = sum(list(map(lambda x : 1/x, dados)))
-    print(round(numerador/denominador))
+    return(round(numerador/denominador))
     
 
 def media_das_taxas(dados_1, dados_2):
     taxa_1 = sum(list(dados_1))/len(dados_1)
     taxa_2 = sum(list(dados_2))/len(dados_2)
-    print (round(((taxa_1/taxa_2)*100), 1))
+    return (round(((taxa_1/taxa_2)*100), 1))
 
 
 def media_das_taxas_constante(dados_1, constante):
     soma = sum(list(dados_1))
     denominador = constante*len(dados_1)
-    print(((1/denominador)*soma)*100)
+    return(((1/denominador)*soma)*100)
+
+
+def moda(dados):
+    return(max(set(dados), key=dados.count))
 
 
 #implementar de modo que ja receba um array def plotar_histograma(array):
@@ -55,4 +60,4 @@ def plotar_histograma():
     plt.show()
 
 
-media_das_taxas_constante([3.0, 5.0, 4.2, 5.2, 8.6], 100)
+print(moda([2, 4, 5, 3, 3, 2, 2, 2, 3, 4, 6, 7, 8, 9, 9, 9, 9, 9]))
