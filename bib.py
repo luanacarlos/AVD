f2864942c8b58a47aef99ba3ce38d709aa9ae7a6
Mateus Mota <motamatheus10@gmail.com>
2023-08-13 22:12:15 -0400

    feito até intervalo de confiança

--- bib.py
+++ bib.py
@@ -1,8 +1,8 @@
 import numpy as np
 from numpy import *
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from collections import Counter
-
+import math
 
 def media_aritmetica(dados):
     soma = 0
@@ -59,5 +59,64 @@ def plotar_histograma():
     plt.hist(frequencias, rwidth=0.5)
     plt.show()
 
+def amplitude (dados):
+    return max(dados) - min(dados)
+        
+def media_amostral(dados):
+    soma = sum(dados)
+    tamanho = len(dados)
+    return soma/tamanho
 
-print(moda([2, 4, 5, 3, 3, 2, 2, 2, 3, 4, 6, 7, 8, 9, 9, 9, 9, 9]))
+def variancia_amostral(dados):
+    n = len(dados)
+    media = media_aritmetica(dados)
+    soma_diferencas_quad = np.sum(np.fromiter(((x - media) * (x - media) for x in dados), dtype=float))
+    variancia = soma_diferencas_quad / (n - 1)
+    return variancia
+
+def desvio_padrao (dados):
+    variancia = variancia_amostral(dados)
+    desvioPadrao = math.sqrt(variancia)
+    return desvioPadrao
+
+
+#Essa função ainda n funciona como deveria!! -> ta sim!!
+def coeficiente_de_variacao(dados):
+    desvio = desvio_padrao(dados)
+    media = media_aritmetica(dados)
+    coefieciente = (desvio/media) *100
+    return coefieciente
+
+def calcular_quartis(dados):
+    dados_ordenados = sorted(dados)
+    n = len(dados_ordenados)
+    
+    quartil_1 = dados_ordenados[int(n * 0.25)]
+    quartil_2 = dados_ordenados[int(n * 0.5)]
+    quartil_3 = dados_ordenados[int(n * 0.75)]
+    
+    return quartil_1, quartil_2, quartil_3
+
+def amplitude_interquartil(dados):
+    q1,q2,q3 = calcular_quartis(dados)
+    a = q1
+    b= q3
+    return b-a
+
+idadePessoas = [3,5,8,11,12]
+
+
+#print(variancia_amostral(idadePessoas))
+print(desvio_padrao(idadePessoas))
+print(media_aritmetica(idadePessoas))
+
+print(coeficiente_de_variacao(idadePessoas))
+
+
+amostra = [10, 15, 20, 25, 30, 35, 40, 45, 50]
+q1, q2, q3 = calcular_quartis(amostra)
+print("Primeiro Quartil (Q1):", q1)
+print("Segundo Quartil (Q2, Mediana):", q2)
+print("Terceiro Quartil (Q3):", q3)
+
+print(amplitude_interquartil(amostra))
