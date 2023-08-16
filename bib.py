708417a32238a268f7faee6b643297c0f7c501bc
luanacarlos <luan.alencar@icomp.ufam.edu.br>
2023-08-16 12:58:50 -0400

    Tentando corrigir intervalo de confianca

--- bib.py
+++ bib.py
@@ -6,7 +6,7 @@ import math
 
 
 def media_aritmetica(dados):
-    return sum(dados)/len(dados) if len(dados) > 0 else 0
+    return round((sum(dados)/len(dados)),3) if len(dados) > 0 else 0
 
 
 def mediana(dados):
@@ -21,7 +21,7 @@ def mediana(dados):
 def media_ponderada(dados, pesos):
     numerador = sum(list(map(lambda x, y: x*y, pesos, dados)))
     media = numerador/sum(pesos)
-    return(round(media,1))
+    return(round(media,3))
 
 
 def media_geometrica(dados):
@@ -32,19 +32,19 @@ def media_geometrica(dados):
 def media_harmonica(dados):
     numerador = len(dados)
     denominador = sum(list(map(lambda x : 1/x, dados)))
-    return(round(numerador/denominador))
+    return(round(numerador/denominador), 3)
     
 
 def media_das_taxas(media, taxa):
     media_1 = sum(list(media))/len(media)
     media_2 = sum(list(taxa))/len(taxa)
-    return (round(((media_2/media_1)*100), 1))
+    return (round(((media_2/media_1)*100), 3))
 
 
 def media_das_taxas_constante(dados_1, constante):
     soma = sum(list(dados_1))
     denominador = constante*len(dados_1)
-    return(((1/denominador)*soma)*100)
+    return(round((((1/denominador)*soma)*100), 3))
 
 
 def moda(dados):
@@ -71,13 +71,13 @@ def variancia_amostral(dados):
     media = media_aritmetica(dados)
     soma_diferencas_quad = np.sum(np.fromiter(((x - media) * (x - media) for x in dados), dtype=float))
     variancia = soma_diferencas_quad / n
-    return variancia
+    return round(variancia, 3)
 
 
 def desvio_padrao(dados):
     variancia = variancia_amostral(dados)
     desvioPadrao = math.sqrt(variancia)
-    return desvioPadrao
+    return round(desvioPadrao, 3)
 
 
 #Essa função ainda n funciona como deveria!! -> ta sim!!
@@ -85,7 +85,7 @@ def coeficiente_de_variacao(dados):
     desvio = desvio_padrao(dados)
     media = media_aritmetica(dados)
     coefieciente = (desvio/media) *100
-    return coefieciente
+    return round(coefieciente, 3)
 
 
 def calcular_quartis(dados):
@@ -120,7 +120,7 @@ def intervalo_de_confianca_menor_30(dados, nivel_de_confianca):
     alpha_barra = 1 - (alpha/2)
     t_student = scipy.stats.t.ppf(alpha_barra, (len(dados)-1))
     constante = t_student*(desvio_padrao(dados)/(math.sqrt(len(dados))))
-    return[media_aritmetica(dados)-constante, media_aritmetica(dados)+constante]
+    return[round((media_aritmetica(dados)-constante), 3), round((media_aritmetica(dados)+constante), 3)]
 
 
 def intervalo_de_confianca(dados, nivel_de_confianca):
@@ -146,25 +146,52 @@ def teste_media_zero(intervalo):
         print(f'Há diferença entre os dois sistemas pois como vemos {intervalo}, o 0 não está incluso')
 
     
-def amostras_nao_pareadas(amostra_A, amostra_B, nivel_de_confianca):
-    media_A =  media_amostral(amostra_A)
-    media_B = media_amostral(amostra_B)
-    diferenca_medias = media_A - media_B
-    desvio_A = desvio_padrao(amostra_A)
-    desvio_B = desvio_padrao(amostra_B)
-    constante = ((desvio_A**2)/len(amostra_A)) + ((desvio_B**2)/len(amostra_B))
-    desvio_das_diferencas = math.sqrt(constante)
-    variavel_a = (1/(len(amostra_A)-1))*(((desvio_A**2)/len(amostra_A)))
-    variavel_b = (1/(len(amostra_B)-1))*(((desvio_B**2)/len(amostra_B)))
-    graus_de_liberdade = (int)(((constante**2)/(variavel_a+variavel_b))-2)
-    confianca = (nivel_de_confianca/100)
+def intervalo_nao_paralelo(diferenca_amostral, diferenca_desvio_padrao, grau_liberdade, nivel_de_confianca):
+    confianca = round((nivel_de_confianca/100),2)
     alpha = 1 - confianca
-    alpha_barra = 1 - (alpha/2)
-    return [diferenca_medias - (desvio_das_diferencas*scipy.stats.t.ppf(graus_de_liberdade, alpha_barra)),
-            diferenca_medias + (desvio_das_diferencas*scipy.stats.t.ppf(graus_de_liberdade, alpha_barra))]
+    alpha_barra = round(((1+confianca)/2), 3)
+    print(f'alpga barra {alpha_barra}')
+    print(scipy.stats.t.ppf(alpha_barra, abs(grau_liberdade)))
+    return [(diferenca_amostral - (diferenca_desvio_padrao*scipy.stats.t.ppf(alpha_barra, abs(grau_liberdade)))), 
+            (diferenca_amostral + (diferenca_desvio_padrao*scipy.stats.t.ppf(alpha_barra, abs(grau_liberdade))))]
+
+
+def amostras_nao_pareadas(amostra_A, amostra_B, nivel_de_confianca):
+    media_amostral_A = media_amostral(amostra_A)
+    media_amostral_B = media_amostral(amostra_B)
+    diferenca_media_amostral = (media_amostral_A - media_amostral_B)
+    print(diferenca_media_amostral)
+    desvio_padrao_A = desvio_padrao(amostra_A)
+    desvio_padrao_B = desvio_padrao(amostra_B)
+    print(desvio_padrao_A)
+    print(desvio_padrao_B)
+    constante_A = round(((desvio_padrao_A**2)/len(amostra_A)), 3)
+    constante_B = round(((desvio_padrao_B**2)/len(amostra_B)), 3)
+    print(f'constante A -> {constante_A}')
+    print(f'constante B -> {constante_B}')
+    desvio_padrao_das_diferencas = round((math.sqrt(constante_A+constante_A)),3)
+    numerador_liberdade = (constante_A+constante_B)**2
+    print(desvio_padrao_das_diferencas)
+    variavel_A = abs(round(((1/len(amostra_A)-1)*(constante_A**2)), 3))
+    variavel_B = abs(round(((1/len(amostra_B)-1)*(constante_B**2)), 3))
+    print(variavel_A)
+    print(variavel_B)
+    grau_de_liberdade = round((numerador_liberdade/(variavel_A+variavel_B)))
+    print(f'grau de liberdade {grau_de_liberdade}')
+    return intervalo_nao_paralelo(diferenca_media_amostral, desvio_padrao_das_diferencas, grau_de_liberdade, nivel_de_confianca)
 
 
 
+amostra_A = [5.3, 16, 0.6, 1.4, 0.6, 7.7, 3.6, 2.4, 12,
+             6, 57, 2, 1, 4, 6, 4, 8, 1]
+
+amostra_B = [19, 3.5, 3.3, 2.5, 3.6, 1.7, 12, 2, 8, 1, 1, 4]
+
+print(amostras_nao_pareadas(amostra_A, amostra_B, 90) )
+
+
+
+'''
 sistema_A = [5.4, 16.6, 0.6, 1.4, 0.6, 7.3]
 sistema_B = [19.1, 3.5, 3.4, 2.5, 3.6, 1.7]
 teste_media_zero(intervalo_de_confianca(subtrai_sistemas(sistema_A, sistema_B), 90)) 
@@ -173,7 +200,7 @@ teste_media_zero(subtrai_sistemas(sistema_A, sistema_B), 99)
 
 exercicio_dois = [1.5, 2.6, -1.8, 1.3, -0.5, 1.7, 2.4] 
 teste_media_zero(exercicio_dois, 99)
-
+'''
 
 def calcula_distribuicao_normal_cdf(z):
     t = 1.0 / (1.0 + 0.2316419 * abs(z))
@@ -210,7 +237,7 @@ alternativa = 'dois-lados'
 
 # Chamada da função
 resultado = teste_hipotese_z(media_amostral, media_nula, desvio_padrao_populacional, tamanho_amostra, nivel_significancia, alternativa)
-print(resultado)
+#print(resultado)
 
 
 
