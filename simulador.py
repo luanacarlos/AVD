47ae1d8f3afb0669efd8345abf1a484ea823b12a
luanacarlos <luan.alencar@icomp.ufam.edu.br>
2023-08-30 10:42:37 -0400

    começando o simulador

---
+++ simulador.py
@@ -0,0 +1,6 @@
+from bib import *             # importar apenas as funcoes que serao utilizadas
+
+
+#fazer um simulador que receba taxas e possa receber uma funcao para ser utilizada durante a simulação
+def simulador(quantidade_elementos, taxa_chegada, taxa_saida, funcao_suporte=0):
+    
