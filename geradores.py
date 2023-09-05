eea8a93524e340d2c21632f522e52dc267e75b8e
luanacarlos <luan.alencar@icomp.ufam.edu.br>
2023-09-04 21:47:03 -0400

    adicionando metodo quadrado

--- geradores.py
+++ geradores.py
@@ -1,25 +1,25 @@
 from bib import *             # importar apenas as funcoes que serao utilizadas
 
 
-def conta_digitos(numero):
-    contador = 0
-    while numero != 0:
-        numero //= 10
-        contador += 1
-    return contador
-
 def preenche_digitos(numero):
     numero = str(numero)
     return numero.zfill(8)
+
+
+def trata_numero(numero):
+    if len(str(numero)) > 8:
+        return str(numero)[:8:]
+    else:
+        return str(numero)
 
 
 def metodo_quadrado_central(z0, quantidade):
     lista = []
     iteracoes = 0
     while iteracoes < quantidade:
-        z0 = (z0**2) if conta_digitos(z0**2) < 8 else preenche_digitos(z0**2)
+        z0 = trata_numero((z0**2)) if len(str(z0**2)) >= 8 else preenche_digitos(z0**2)
         z1 = str(z0[2:6:])
-        z0 = z1
+        z0 = int(z1)
         z1 = int(z1) / 10000.0
         lista.append(z1)
         iteracoes += 1
@@ -27,5 +27,9 @@ def metodo_quadrado_central(z0, quantidade):
     return lista
     
     
-print(metodo_quadrado_central(7182, 5))
+def gerador_linear_congruencial():
+    
+    
+print(metodo_quadrado_central(1234, 30))
+
         
