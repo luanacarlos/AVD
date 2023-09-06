b565cad77dcf2321d7065a5ad779fd71f2476ba1
Mateus Mota <motamatheus10@gmail.com>
2023-09-06 17:04:06 -0400

    chi-quadrado

--- geradores.py
+++ geradores.py
@@ -1,5 +1,6 @@
 from bib import *             # importar apenas as funcoes que serao utilizadas
 import random         # Gerador de NA do Python
+from scipy.stats import chi2
 
 
 def preenche_digitos(numero):
@@ -54,11 +55,52 @@ def gerador_python(semente, quantidade):
         
     return lista
         
+def chi_squared_test(generator_output, k):
+    # Gerar k intervalos igualmente espaçados entre 0 e 1
+    intervals = np.linspace(0, 1, k+1)
     
+    # Calcular as frequências observadas
+    observed_frequencies = np.histogram(generator_output, bins=intervals)[0]
+    
+    # Calcular as frequências esperadas (uniformemente distribuídas)
+    n = len(generator_output)
+    expected_frequency = n / k
+    
+    expected_frequencies = np.array([expected_frequency] * k)
+    
+    # Calcular a estatística do qui-quadrado
+    chi_squared = np.sum((observed_frequencies - expected_frequencies)**2 / expected_frequencies)
+    
+    # Calcular os graus de liberdade
+    df = k - 1
+    
+    # Encontrar o valor crítico do qui-quadrado para alpha=0.05 e df graus de liberdade
+    critical_value = chi2.ppf(0.95, df)
+    
+    # Comparar o valor calculado de chi_squared com o valor crítico
+    if chi_squared < critical_value:
+        return "Aceita a hipotese nula. Os numeros sao aleatorios."
+    else:
+        return "Rejeita a hipotese nula. Os numeros nao sao aleatorios."
+    
+# Parâmetros para o gerador G1
+a1 = 12351
+c1 = 1
+m1 = 2**15
+quantidade1 = 10000  # Ajuste a quantidade conforme necessário
+
+# Gerar a sequência de números pseudoaleatórios
+output1 = metodo_quadrado_central(7182, 1000)
+
+# Realizar o teste do qui-quadrado
+k1 = 10  # Número de intervalos
+result1 = chi_squared_test(output1, k1)
+print(result1)
+
   
 #print(metodo_quadrado_central(1234, 30))
 #numeros = gerador_linear_congruencial(25214903917, 11, 2**48, 5000)
-print(gerador_python(123, 100))
+#print(gerador_python(123, 100))
 
 """import matplotlib.pyplot as plt
 
