79c42d70221ac8fe4aa5631a27768214fd500ff4
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2023-10-13 16:05:23 -0400

    para k = 3, repetição = 1 -> funcionando

--- 2kFATORIAL/main.py
+++ 2kFATORIAL/main.py
@@ -124,27 +124,6 @@ def imprimir_tabela_2_repeat_maior1(tabela):
             print("{:^5} {:^5} {:^5} {:^5} {:^5} {:^10}".format(linha[0], linha[1], linha[2], linha[3], str(linha[4]), linha[5]))
 
 
-#teste para k = 2
-k = int(input("Digite o valor de K: "))
-repeticoes = int(input("Digite o valor de repeticoes: "))
-if k == 2:
-    if repeticoes == 1:
-        tabela = tabela_de_sinais_2(repeticoes)
-        adicionar_resultado_repeat_1(tabela)
-        imprimir_tabela_2_repeat_1(tabela)
-    else:
-        tabela = tabela_de_sinais_2(repeticoes)
-        calcula_yMedia(tabela, repeticoes)
-        adicionar_resultado_repeat_maior1(tabela)
-        imprimir_tabela_2_repeat_maior1(tabela)
-else:
-    print("TODO()")
-
-
-
-
-
-
 # PARA K = 3
 def tabela_de_sinais3(k,repeticoes):
     tabela = []
@@ -155,10 +134,17 @@ def tabela_de_sinais3(k,repeticoes):
         linha.append(linha[2] * linha[3])              # BC
         linha.append(linha[1] * linha[2] * linha[3])   # ABC
         if repeticoes > 1:
-            linha.append((0,) * repeticoes) #Y()
+            count = 0
+            a = [15,12,18]
+                # while(count<repeticoes):
+                # x = int(input("Digite o valor de resultados: "))
+                # a.append(x)
+                # count = count + 1  
+            linha.append(a) # Y(1,2,3,4,5,6,7,8)
         else:
-            linha.append(0)      
-        tabela.append(linha) # Y
+            x = int(input("Digite o valor de resultados: "))
+            linha.append(x) # Y   
+        tabela.append(linha) # 
     return tabela
 
 
@@ -169,5 +155,91 @@ def imprimir_tabela_3(tabela):
     for linha in tabela:
         print ("{:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^5}".format(linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[8]))
 
+def imprimir_tabela_3_maior(tabela):
+    print("\n----------Tabela De Sinais--------------\n")
+    print ("{:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^25}".format("1","A", "B", "C", "AB", "AC", "BC", "ABC", "Y", "yMedia"))
+    for linha in tabela:
+        print ("{:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^5} {:^10}".format(linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], str(linha[8]), linha[9]))
 
 
+
+# Adicionar resultados - Repeticoes maior que 1
+def calcula_yMedia(tabela, repeticoes):
+    soma_y = 0
+    for linha in tabela:
+        if len(linha) > 8: #Coluna Y
+            soma_y = sum(linha[8])/ repeticoes
+            linha.append(soma_y) #Coluna Y Media
+
+k = 3
+tabela = tabela_de_sinais3(3,1)
+imprimir_tabela_3(tabela)
+# adicionar_resultado_3_repeat_1(tabela)
+
+##FUNCAO PARA CALCULAR OS RESULTADOS E COLOCAR NA TABELA - REPETIÇÔES 
+y = []
+
+# Multiplicar a coluna Y pela coluna 1
+sum = 0
+for i, linha in enumerate(tabela):
+    sum = sum + (tabela[i][-1] * linha[0])
+y.append(sum)
+
+#COLUNA A*Y 
+sum = 0
+for i, linha in enumerate(tabela):
+    sum = sum + (tabela[i][-1] * linha[1])
+y.append(sum)
+
+#COLUNA B*Y
+sum = 0
+for i, linha in enumerate(tabela):
+    sum = sum + (tabela[i][-1] * linha[2])
+y.append(sum)
+
+#COLUNA C*Y
+sum = 0
+for i, linha in enumerate(tabela):
+    sum = sum + (tabela[i][-1] * linha[3])
+y.append(sum)
+
+#COLUNA AB*Y
+sum = 0
+for i, linha in enumerate(tabela):
+    sum = sum + (tabela[i][-1] * linha[4])
+y.append(sum)
+
+#COLUNA AC*Y
+sum = 0
+for i, linha in enumerate(tabela):
+    sum = sum + (tabela[i][-1] * linha[5])
+y.append(sum)
+
+#COLUNA BC*Y
+sum = 0
+for i, linha in enumerate(tabela):
+    sum = sum + (tabela[i][-1] * linha[6])
+y.append(sum)
+
+#COLUNA ABC*Y
+sum = 0
+for i, linha in enumerate(tabela):
+    sum = sum + (tabela[i][-1] * linha[7])
+y.append(sum)
+
+#SÓ COLOCAR TOTAL lA MESMO
+y.append("Total")
+tabela.append(y)
+
+#lINHA ABAIXO QUE TEM OS RESULTAODS DIVIDIDO POR 4
+new_y = []
+for i in range (len(y)-1):
+    new_y.append(y[i] /2**k)
+new_y.append("Total/4")
+tabela.append(new_y)  
+
+
+
+print(tabela)
+
+imprimir_tabela_3(tabela)
